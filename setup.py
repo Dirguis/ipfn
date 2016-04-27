7af9e9f66c693e812085f43d43a5e9ecbc395562
Dirguis <damien2227@hotmail.com>
2016-04-26 22:10:26 -0400

    add check to make sure no division by 0 occur in the pd version of the code

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.3',
+    version='1.1.3',
 
     description='Iterative Proportional Fitting with N dimensions, for python',
     long_description=long_description,
@@ -43,7 +43,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
