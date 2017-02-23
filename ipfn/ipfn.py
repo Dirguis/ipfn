0df696731a114463e7e30822a81d411e6a306fc9
harisbal <theoballisgr@gmail.com>
2017-02-23 19:42:14 +0000

    Checking -  Minor updates
    Tested and Working

--- ipfn/ipfn.py
+++ ipfn/ipfn.py
@@ -152,7 +152,7 @@ class ipfn(object):
         print(df)
         print(df.groupby('age')['total'].sum(), xip)"""
 
-        s = df.copy()
+        s = df
         aggrs = self.aggregates
         dims = self.dimensions
         factors = []
@@ -208,8 +208,9 @@ class ipfn(object):
                 i += 1
                 # print(i, conv)
 
-        df = self.original
-        df[self.values_column] = m.values
+        #Reset to dataframe
+        m.name = self.values_column
+        df = m.reset_index()
 
         converged = 1
         if i <= self.max_itr:
@@ -443,5 +444,5 @@ if __name__ == '__main__':
     df = ipfn_df.iteration()
 
     print(df)
-    print(df.groupby(level='size').sum(), xpjp)
-    #print(df.groupby(level=['size','age']).sum(), xpjk)    
+    print(df.groupby(['size','age']).sum(), xpjk)
+
